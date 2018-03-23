0205e02dd092d81f7c1029b25e42c59c03739172
DimmNet <30796320+DimmNet@users.noreply.github.com>
2018-03-23 08:54:36 +0200

    Навожу красоту.

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,21 @@ Route::redirect('/', '/home');
 Auth::routes();
 
 Route::get('/home', 'TasksController@showUserTasks')->name('home');
-Route::get('/all', 'TasksController@index')->name('tasks.all');
+Route::get('/all', 'TasksController@showAllTasks')->name('tasks.all');
+
 Route::get('/task/{task}/{title}', 'TasksController@show')->name('tasks.show')
     ->where('task', '\d+');
+
 Route::get('/task/create', 'TasksController@create')->name('tasks.create');
 Route::post('/task', 'TasksController@store')->name('tasks.store');
+
 Route::get('/task/complete/{task}', 'TasksController@complete')->name('tasks.complete')
     ->where('task', '\d+')->middleware('can:update,task');
+
 Route::get('/task/edit/{task}', 'TasksController@edit')->name('tasks.edit')
     ->where('task', '\d+')->middleware('can:update,task');
 Route::post('/task/update/{task}', 'TasksController@update')->name('tasks.update')
     ->where('task', '\d+')->middleware('can:update,task');
+
 Route::delete('task/delete/{task}', 'TasksController@destroy')->name('tasks.delete')
     ->where('task', '\d+')->middleware('can:delete,task');
